7aec6ae3a8b1228bdabd24d7ed7fed89dc151ef5
NikolayDvoinikh <108171672+NikolayDvoinikh@users.noreply.github.com>
2024-06-18 08:40:59 +0300

    fix dark mode in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -130,6 +130,7 @@ const config: Config = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
+  darkMode: "class",
   plugins: [],
 };
 export default config;
